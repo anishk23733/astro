582d27285d8fd432a8ad371bb24cc2e26630661e
Anish Kachinthaya <anishk23733@gmail.com>
2020-11-05 09:27:16 -0800

    minor

---
+++ public/electron.js
@@ -0,0 +1,124 @@
+const { app, screen, BrowserWindow, Menu } = require("electron");
+const isDev = require("electron-is-dev");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+  // Create the browser window.
+  let [newWidth, newHeight] = [
+    Math.floor(0.5 * width) > 750 ? Math.floor(0.5 * width) : 750,
+    Math.floor(0.75 * height) > 600 ? Math.floor(0.75 * height) : 600,
+  ];
+  mainWindow = new BrowserWindow({
+    width: newWidth,
+    height: newHeight,
+    minHeight: 600,
+    minWidth: 750,
+    titleBarStyle: "hiddenInset",
+    resizable: true,
+    webPreferences: {
+      nativeWindowOpen: true,
+    },
+    // "Icon made by Freepik from www.flaticon.com"
+    // icon: path.join(__dirname, "public/astro/icon_256x256.png"),
+  });
+
+  const startURL = isDev
+    ? "http://localhost:3000"
+    : `file://${path.join(__dirname, "../build/index.html")}`;
+  if (isDev) mainWindow.webContents.openDevTools();
+  mainWindow.loadURL(startURL);
+
+  mainWindow.once("ready-to-show", () => mainWindow.show());
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+function createMenu() {
+  const application = {
+    label: "Astro",
+    submenu: [
+      {
+        label: "About Astro",
+        selector: "orderFrontStandardAboutPanel:",
+      },
+      {
+        type: "separator",
+      },
+      {
+        label: "Quit",
+        accelerator: "Command+Q",
+        click: () => {
+          app.quit();
+        },
+      },
+    ],
+  };
+
+  const edit = {
+    label: "Edit",
+    submenu: [
+      {
+        label: "Undo",
+        accelerator: "CmdOrCtrl+Z",
+        selector: "undo:",
+      },
+      {
+        label: "Redo",
+        accelerator: "Shift+CmdOrCtrl+Z",
+        selector: "redo:",
+      },
+      {
+        type: "separator",
+      },
+      {
+        label: "Cut",
+        accelerator: "CmdOrCtrl+X",
+        selector: "cut:",
+      },
+      {
+        label: "Copy",
+        accelerator: "CmdOrCtrl+C",
+        selector: "copy:",
+      },
+      {
+        label: "Paste",
+        accelerator: "CmdOrCtrl+V",
+        selector: "paste:",
+      },
+      {
+        label: "Select All",
+        accelerator: "CmdOrCtrl+A",
+        selector: "selectAll:",
+      },
+    ],
+  };
+
+  const template = [application, edit];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+}
+
+app.on("ready", () => {
+  createWindow();
+  createMenu();
+});
+
+app.on("window-all-closed", () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
--- src/App.js
+++ src/App.js
@@ -1,17 +1,206 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useMemo, useState, useCallback } from "react";
+import React from "react";
+import { createEditor } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
+import axios from "axios";
+import Sidebar from "./Components/Sidebar";
+import Logo from "./Assets/png_style.svg";
+import firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectinData } from "react-firebase-hooks/firestore";
+
+const uuid = require("uuid");
+const syllable = require("syllable");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA4MXsbkGLcs921XbDX89w8USLYFy90Meg",
+  authDomain: "astro-7.firebaseapp.com",
+  databaseURL: "https://astro-7.firebaseio.com",
+  projectId: "astro-7",
+  storageBucket: "astro-7.appspot.com",
+  messagingSenderId: "646788483761",
+  appId: "1:646788483761:web:e202af8abcfdd195dcf090",
+  measurementId: "G-9JJXXZWQXC",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+let returnSyllableComponent = (children) => {
+  let line = children.props.node.children[0].text;
+  let numSyllables = syllable(line);
+  let outcome = line === "break";
+  return (
+    <span
+      className={outcome ? "emptyLine" : "syllables"}
+      data-slate-editor
+      contentEditable={false}
+    >
+      {outcome ? "" : numSyllables}
+    </span>
+  );
+};
+
+const items = [
+  { name: "account", label: "Account" },
+  {
+    name: "songs",
+    label: "Songs",
+    items: [
+      { name: "sickoMode", label: "SICKO MODE" },
+      { name: "goosebumps", label: "goosebumps" },
+      { name: "create", label: "+ New" },
+    ],
+  },
+];
 
 function App() {
+  const editor = useMemo(() => withReact(createEditor()), []);
+  const [value, setValue] = useState([
+    {
+      type: "paragraph",
+      children: [{ text: "Start writing here." }],
+    },
+  ]);
+  // const [user] = useAuthState(auth);
+  let [user, setUser] = useState(false);
+
+  const [rhymeWords, setrhymeWords] = useState("");
+  let searchRhymes = async (word) => {
+    axios
+      .all([
+        axios.get(`https://api.datamuse.com/words?rel_rhy=${word}`),
+        axios.get(`https://api.datamuse.com/words?rel_nry=${word}`),
+      ])
+      .then(
+        axios.spread((res1, res2) => {
+          // let data = res1.data.concat(res2.data);
+          let elems = [];
+          res1.data.forEach((wordSet) => {
+            elems.push(<div className="rhymeWord">{wordSet.word}</div>);
+          });
+          res2.data.forEach((wordSet) => {
+            elems.push(<div className="rhymeWordNear">{wordSet.word}</div>);
+          });
+          setrhymeWords(elems);
+        })
+      );
+  };
+  const renderElement = useCallback(({ attributes, children, element }) => {
+    return (
+      <div className="line" {...attributes}>
+        {returnSyllableComponent(children)}
+        {children}
+      </div>
+    );
+  }, []);
+  let signInPage = new SignIn();
+  let [page, updatePage] = useState(
+    <div className="signInPage">
+      <div className="signInContainer" contentEditable="false">
+        <img className="signInLogo" src={Logo}></img>
+        <h1>Astro</h1>
+        <SignIn ref={signInPage}></SignIn>
+      </div>
+    </div>
+  );
+
+  if (signInPage.signIn) {
+    return page;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          We outta this world
-        </p>
-      </header>
+      <Sidebar items={items}></Sidebar>
+      <div className="container">
+        <div className="textEditor">
+          <Slate
+            editor={editor}
+            value={value}
+            onChange={(newValue) => {
+              if (editor.getFragment()[0] !== undefined) {
+                let highlighted = editor.getFragment()[0].children[0].text;
+                if (highlighted.length > 0)
+                  console.log(searchRhymes(highlighted));
+                setValue(newValue);
+                let i;
+                for (i = 0; i < newValue.length; i++) {
+                  let line = newValue[i].children[0].text;
+                  console.log(syllable(line), line);
+                }
+              }
+            }}
+          >
+            <Editable renderElement={renderElement} />
+          </Slate>
+        </div>
+        <div contentEditable={false} className="rhymeContainer">
+          {rhymeWords}
+        </div>
+      </div>
     </div>
   );
 }
 
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signIn: false,
+      register: false,
+    };
+
+    this.SignIn = () => {
+      this.setState({ signIn: true });
+    };
+    this.RegisterPage = () => {
+      this.setState({ register: true });
+    };
+  }
+  render() {
+    return (
+      <div>
+        <p>Welcome back!</p>
+        <div className="signInForm">
+          <input placeholder="Username"></input>
+          <input placeholder="Password"></input>
+          <button
+            onClick={() => {
+              this.SignIn();
+            }}
+          >
+            Login
+          </button>
+          <p>
+            Not a returning user? <a href="">Register</a>.
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+function Register() {
+  return (
+    <div className="signInPage">
+      <div className="signInContainer" contentEditable="false">
+        <img className="signInLogo" src={Logo}></img>
+        <h1>Astro</h1>
+        <p>Welcome back!</p>
+        <div className="signInForm">
+          <input placeholder="Username"></input>
+          <input placeholder="Password"></input>
+          <button>Login</button>
+          <p>
+            Not a returning user? <a href="">Register</a>.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default App;
